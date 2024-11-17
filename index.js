6c0034d576d08bc81989802e2e37ce5b2d05c934
Hursh Bajaj <hurshbajaj2011@gmail.com>
2024-11-17 06:52:07 +0530

    signUp page complete

---
+++ index.js
@@ -0,0 +1,74 @@
+const cards = document.querySelectorAll(".card");
+const content = document.querySelectorAll(".content")
+const main = document.getElementsByClassName("main")
+const content1 = document.querySelectorAll(".A")
+const content2 = document.querySelectorAll(".B")
+const content3 = document.querySelectorAll(".C")
+
+function hideAllContent() {
+    content1.forEach(content => content.style.opacity="0");
+    content2.forEach(content => content.style.opacity="0");
+    content3.forEach(content => content.style.opacity="0");
+}
+hideAllContent()
+cards.forEach(card => {
+
+    console.log("hovered");
+    card.addEventListener("click", (event) => {
+        if(card.style.transform == "translateX(730px)" || card.style.transform == "translate(730px, -100px)"){
+            goBack()
+            
+        }
+        else{
+            goBack()
+            goIn(card)
+        }
+         
+        
+        
+    });
+
+});
+
+function goBack(){
+    cards.forEach(card=>{
+        card.style.transform = "translateX(0px)";
+        
+        card.style.backgroundColor = "#9073be73";
+        card.style.opacity = "1";
+    })
+    main[0].style.transform = "translateY(0px)";
+
+    hideAllContent()
+   
+}
+function goIn(card){
+    
+    card.style.backgroundColor = "#997DC9";
+    card.style.opacity = "0.5";
+    card.style.transform = "translateX(730px)";   
+    if(card == cards[1]){
+        card.style.transform = "translate(730px, -100px)";   
+    }
+    main[0].style.transform = "translateY(620px)";
+    switch(card){
+        case cards[0]:
+            console.log("!!!")
+            content1.forEach(content =>{
+                content.style.opacity = "1";
+            })
+            break;
+        case cards[1]:
+            content2.forEach(content =>{
+                content.style.opacity = "1";
+            })
+            break;
+        case cards[2]:
+            content3.forEach(content =>{
+                content.style.opacity = "1";
+            })
+            break;
+
+    }
+    
+}
